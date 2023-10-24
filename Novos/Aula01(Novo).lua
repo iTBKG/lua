42fbde9119a942095a271f8adfb1cedd2e434b15
Érick Dala <74063563+iTBKG@users.noreply.github.com>
2023-10-24 19:46:27 -0400

    Add files via upload

---
+++ Novos/Aula01(Novo).lua
@@ -0,0 +1,18 @@
+
+-- Exibe o texto no output (saída)
+print("Olá Mundo!")
+
+-- Estamos exibindo números em forma de string(texto)
+print("123456")
+
+-- Estamos exibindo número de forma direto, em number
+print(123456)
+
+-- Essas linhas com esses "-- tracinhos" no começo, são utilizadas para fazer comentários em seu código
+-- É de EXTREMA IMPORTÂNCIA deixar seu código comentado
+
+--[[
+   Se utilizarmos "tracinhos" e colchetes
+   podemos fazer um comentário
+   de múltiplas linhas
+]]--
